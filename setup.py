57b80d98599b02b6970afc0388b0b050df6d58eb
Igor Brylyov <movefasta@dezcom.org>
2023-03-01 00:04:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages  # type: ignore
-import unified_planning
+# import unified_planning
 
 
 long_description = """============================================================
